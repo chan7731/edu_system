4c246020e948ee7098841ff4bc4ef6580de6d09c
박찬혁 <cksgurrhtn@naver.com>
2025-07-07 10:49:40 +0900

    edu system commit

---
+++ src/main/java/com/chan7731/edu_system_2/controller/StudentController.java
@@ -0,0 +1,46 @@
+package com.chan7731.edu_system_2.controller;
+
+import com.chan7731.edu_system_2.model.Student;
+import com.chan7731.edu_system_2.model.Teacher;
+import com.chan7731.edu_system_2.repository.StudentRepository;
+import com.chan7731.edu_system_2.repository.TeacherRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/students")
+@RequiredArgsConstructor
+public class StudentController {
+    private final StudentRepository studentRepository;
+    private final TeacherRepository teacherRepository;
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("students", studentRepository.findAll());
+
+        return "student-list";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        List<Teacher> teachers = teacherRepository.findAll();
+        model.addAttribute("student", new Student());
+        model.addAttribute("teachers", teachers);
+
+        return "student-form";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute Student student) {
+        studentRepository.save(student);
+
+        return "redirect:/students";
+    }
+}
